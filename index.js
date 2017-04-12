ebe99864835f4842317722b4c2922cc2a9e9754c
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-04-12 14:19:29 +0800

    0.0.3

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function stp(strTmpl, data) {
   if (data !== null && data !== undefined) {
     return stp(strTmpl)(data);
   }
-  return new Function("$", "with($){return `" + strTmpl + '`;}');
+  return new Function("$", "with($){return `" + strTmpl.replace(/`/g, '\\`') + '`;}');
 };
